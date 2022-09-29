112b55f84b67fd49cc9354ec112b79612200b74a
Mahmoud Mamdouh Abd Allah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2022-09-30 01:24:22 +0200

    update deploy.sh

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
 # Deploy to AWS EB
 # cd ./www
-eb init eb-api-1011 --region us-east-1 --platform node.js
-eb use eb-api-1011
+eb init myappdep10 --region us-east-1 --platform node.js
+eb use myappdep10-env
 eb deploy eb-api-1011
